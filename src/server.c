580cf371e3c98014a0111372f8db8a22fa902ad1
Brandon <brandon.cole@bison.howard.edu>
2018-09-25 13:29:48 -0400

    fixed function to read len of file name

---
+++ src/server.c
@@ -0,0 +1,18 @@
+ /* inet (3) funtions */
+#include "server_helper.h"
+
+
+int main(int argc, char *argv[]) {
+	if (argc < 2) {
+		fprintf(stderr, "ERROR, no port provided\n");
+		exit(1);
+	}
+
+	int socket_server_fd = create_server(atoi(argv[1]));
+
+	run_server(socket_server_fd);
+
+	if (close(socket_server_fd) < 0){
+		puts("ERROR CLOSING SERVER");
+	}
+}
